1c7c012ed77ecd28a7affcade88ee7133409290a
hugohil <hugo@soixantecircuits.fr>
2016-02-05 18:02:35 +0100

    set window title

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 //========================================================================
 int main( ){
 	ofSetupOpenGL(512,384,OF_WINDOW);			// <-------- setup the GL context
+  ofSetWindowTitle("ofxSocketIO Example");
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
